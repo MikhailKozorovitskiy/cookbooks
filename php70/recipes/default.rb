ce5b896e14712265efb9bbc54462413e33ad06c2
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-09 21:44:49 -0500

    adding php7.0 and 7.1 repos

---
+++ php70/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: php7.0
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "php-repo"
+
+package 'php7.0' do
+	          action :install
+end
+package 'php7.0-cli' do
+	          action :install
+end
+
+package 'php7.0-curl' do
+	          action :install
+end
+
+package 'php7.0-mysql' do
+	          action :install
+end
+
+package 'php7.0-gd' do
+	          action :install
+end
+
+package 'php7.0-xml' do
+	          action :install
+end
+
+package 'php7.0-mbstring' do
+	          action :install
+end
+
+package 'php7.0-mcrypt' do
+	          action :install
+end
+
---
+++ php71/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: php7.1
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "php-repo"
+
+package 'php7.1' do
+	          action :install
+end
+package 'php7.1-cli' do
+	          action :install
+end
+
+package 'php7.1-curl' do
+	          action :install
+end
+
+package 'php7.1-mysql' do
+	          action :install
+end
+
+package 'php7.1-gd' do
+	          action :install
+end
+
+package 'php7.1-xml' do
+	          action :install
+end
+
+package 'php7.1-mbstring' do
+	          action :install
+end
+
+package 'php7.1-mcrypt' do
+	          action :install
+end
+
