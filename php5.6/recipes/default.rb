9608d7745acf9b6e4a583c52dbb9841d6d2d2dd3
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-09 21:25:04 -0500

    adding php5.6

---
+++ php5.6/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: php5.6
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "php-repo"
+
+package 'php5.6' do
+	  action :install
+end
+package 'php5.6-cli' do
+	  action :install
+end
+
+package 'php5.6-curl' do
+	  action :install
+end
+
+package 'php5.6-mysql' do
+	  action :install
+end
+
+package 'php5.6-gd' do
+	  action :install
+end
+
+package 'php5.6-xml' do
+	  action :install
+end
+
+package 'php5.6-mbstring' do
+	  action :install
+end
+
+package 'php5.6-mcrypt' do
+	  action :install
+end
+
+
