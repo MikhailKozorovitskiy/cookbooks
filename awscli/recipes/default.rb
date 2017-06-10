a6df731da8ef54d59cb0fa3a9c143520efa27e81
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-10 07:35:41 -0500

    updating subdomain creation

--- awscli/recipes/default.rb
+++ awscli/recipes/default.rb
@@ -26,3 +26,18 @@ end
 execute "aws-cli install" do
 	        command "apt-get install awscli -y"
 end
+
+template '/home/ubuntu/aws-dns.json' do
+	   variables('host': node["opsworks"]["instance"]["hostname"],
+		     'domain': node['hosted-domain'],
+	   	     'ip': node["opsworks"]["instance"]["ip"])
+	  source 'aws-dns.erb'
+	  owner 'root'
+	  group 'root'
+	  mode '0755'
+end
+
+execute "aws-cli install" do
+	command "aws route53 change-resource-record-sets --hosted-zone-id {node['hosted-zone-id']} --change-batch file:///home/ubuntu/aws-dns.json"
+end
+
