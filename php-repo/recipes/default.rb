793bf71d649c4b9a2f89969535c8569b00792f85
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-09 21:07:56 -0500

    adding php repo and fixing node index url

---
+++ php-repo/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: php-repo
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "nginx"
+
+apt_repository 'php-repo' do
+	  uri        'ppa:ondrej/php'
+end
+
+execute "apt-get update" do
+	          command "apt-get update"
+end
+
+package 'php' do
+	  action :remove
+end
+
