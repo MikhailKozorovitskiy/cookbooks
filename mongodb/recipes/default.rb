e8b40c8e5bc118216ca27d00ba8c62e9ce9e9a62
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-10 05:44:22 -0500

    readding mongo

---
+++ mongodb/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: mongodb
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute 'mongodb_addkey' do
+	command 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927'
+end
+
+execute 'mongodb_addrepo' do
+	command 'http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse'
+end
+
+execute "mongo apt-get update" do
+	          command "apt-get update"
+end
+
+package 'mongodb-org' do
+	  action :install
+end
+
+cookbook_file "/lib/systemd/system/mongod.service" do
+	source "mongod.service"
+	mode "0644"
+end
+
+execute "mongo restart systemctl" do
+	command "systemctl daemon-reload"
+end
+
+execute "mongo enable service" do
+	command "systemctl start mongod"
+end
+
+execute "mongo start service" do
+	command "systemctl enable mongod"
+end
+
