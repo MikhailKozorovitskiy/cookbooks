8aaa84eedccc79b1647b1ca24a389dce790e80ff
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-10 06:03:01 -0500

    updating mongo repo add code

--- mongodb/recipes/default.rb
+++ mongodb/recipes/default.rb
@@ -7,11 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 execute 'mongodb_addkey' do
-	command 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927'
+	command 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6'
 end
 
 execute 'mongodb_addrepo' do
-	command 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse'
+	command 'echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list'
 end
 
 execute "mongo apt-get update" do
