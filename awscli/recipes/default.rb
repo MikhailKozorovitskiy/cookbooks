0ccc1be22f7ce09b22d2e8493845397d275d3dc3
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-10 08:36:47 -0500

    testing 2

--- awscli/recipes/default.rb
+++ awscli/recipes/default.rb
@@ -29,20 +29,20 @@ end
 
  Chef::Log.fatal("#{node}")
   
-host = node["opsworks"]["instance"]["hostname"]
-domain = node['hosted-domain']
-ip = node["opsworks"]["instance"]["ip"]
-template '/home/ubuntu/aws-dns.json' do
-	   variables({'host': host,
-		     'domain': domain,
-	   	     'ip': ip})
-	  source 'aws-dns.erb'
-	  owner 'root'
-	  group 'root'
-	  mode '0755'
-end
+#host = node["opsworks"]["instance"]["hostname"]
+#domain = node['hosted-domain']
+#ip = node["opsworks"]["instance"]["ip"]
+#template '/home/ubuntu/aws-dns.json' do
+#	   variables({'host': host,
+#		     'domain': domain,
+#	   	     'ip': ip})
+#	  source 'aws-dns.erb'
+#	  owner 'root'
+#	  group 'root'
+#	  mode '0755'
+#end
 
-execute "aws-cli install" do
-	command "aws route53 change-resource-record-sets --hosted-zone-id {node['hosted-zone-id']} --change-batch file:///home/ubuntu/aws-dns.json"
-end
+#execute "aws-cli install" do
+#	command "aws route53 change-resource-record-sets --hosted-zone-id {node['hosted-zone-id']} --change-batch file:///home/ubuntu/aws-dns.json"
+#end
 
