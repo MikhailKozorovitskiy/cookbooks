4faa43c97889f3c0bbb2b4317029146efc104fcb
thecrazyrussian <mikhail@thecrazyrussian.com>
2017-06-10 05:54:35 -0500

    adding aws cli

---
+++ aws-cli/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: aws-cli
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "aws-cli python dependencies" do
+	command "apt-get install python-setuptools python-dev build-essential -y"
+end
+
+execute "aws-cli install pip" do
+	        command "pip install --upgrade pip"
+end
+
+execute "aws-cli dependencies" do
+	        command "pip install --upgrade virtualenv"
+end
+
+execute "aws-cli install" do
+	        command "pip install --upgrade --user awscli"
+end
+
