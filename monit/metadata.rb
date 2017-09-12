78a3af55bf9082900a14ae617589a0f453e226d5
Mikhail Kozorovitskiy <mikhail.kozorovitskiy@gmail.com>
2017-09-11 21:57:06 -0500

    update monit cookbook

---
+++ monit/metadata.rb
@@ -0,0 +1,7 @@
+name             'apt'
+maintainer       'Mikhail Kozorovitskiy'
+maintainer_email 'mikhail@thecrazyrussian.com'
+license          'All rights reserved'
+description      'Installs/Configures apt'
+version          '0.1.0'
+depends          'apt'
---
+++ monit/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: monit
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "apt-get monit" do
+	  command "apt-get install monit"
+end
+
+template '/etc/monit/monitrc' do
+	  source 'monitrc.erb'
+	  owner 'root'
+	  group 'root'
+	  mode '0755'
+end
+
+template '/etc/nginx/sites-enabled/default' do
+	  source 'default.erb'
+	  owner 'root'
+	  group 'root'
+	  mode '0755'
+end
+
+service "monit" do
+  action :restart
+end
+
+service "nginx" do
+  action :restart
+end
