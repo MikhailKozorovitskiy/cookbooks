1e993d6c526342781c4295b80502750cfb50e56a
Mikhail Kozorovitskiy <mikhail.kozorovitskiy@gmail.com>
2017-10-02 11:05:38 -0500

    adding redis install cookbook

---
+++ redis/metadata.rb
@@ -0,0 +1,7 @@
+name             'react'
+maintainer       'Mikhail Kozorovitskiy'
+maintainer_email 'mikhail@thecrazyrussian.com'
+license          'All rights reserved'
+description      'Installs/Configures React'
+version          '0.1.0'
+depends		 'nginx'
---
+++ redis/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: redis# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+package 'redis-server' do
+	          action :install
+end
