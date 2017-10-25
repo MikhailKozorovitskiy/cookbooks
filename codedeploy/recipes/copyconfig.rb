294d2a4e137d5300d8ce46fe66d339fbf9b87620
Mikhail Kozorovitskiy <mikhail.kozorovitskiy@gmail.com>
2017-10-24 22:42:39 -0500

    adding more codedeploy hooks

--- codedeploy/recipes/copyconfig.rb
+++ codedeploy/recipes/copyconfig.rb
@@ -11,11 +11,15 @@ include_recipe "codedeploy"
 stripe_key = node['STRIPE_PUBLISHABLE_KEY']
 base_api_url = node['BASE_API_URL']
 base_url = node['BASE_URL']
+app_domain = node['APP_DOMAIN']
+app_path = node['APP_PATH']
 
 template '/root/.env' do
           variables({'stripe_key': stripe_key,
               'base_api_url': base_api_url,
-              'base_url': base_url})
+              'base_url': base_url,
+              'app_domain': app_domain,
+              'app_path': app_path})
           source 'env.erb'
           owner 'root'
           group 'root'
